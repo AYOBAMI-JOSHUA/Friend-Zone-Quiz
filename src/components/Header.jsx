2bbb48680b17d8a084be3bf982665fab361f40c8
AYOBAMI-JOSHUA <ayobamij89@gmail.com>
2025-05-17 12:59:52 +0100

    added logo and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import logo from './logo.svg';
 
 function Header() {
 
     return (
         <div className="app-header">
             <header >
+              <img src={logo} alt="Friend Zone Quiz" />
               <h1>The Friend Zone Quiz</h1>
             </header>
         </div>
